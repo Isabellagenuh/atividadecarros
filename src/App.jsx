325ca5f5587cdc374fc1827e1af574445bf56f40
Isabella Genuino <isabellageenuino@gmail.com>
2024-03-19 04:24:12 -0300

    quarto commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,20 @@ export default function App() {
       <section>
         <div className="bg-orange">
           <img src={iconSedans} alt="icone do primeiro carro" />
-          <h2>Sedans</h2>
-          <p>Escolha um sedan pelo preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
+          <h2 className="font-shoulders">Sedans</h2>
+          <p className="font-lexend">Escolha um sedan pelo preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
           <a href="#">Ver mais</a>
         </div>
         <div className="bg-green">
           <img src={iconSuvs} alt="carro suv" />
-          <h2>Suvs</h2>
-          <p>Escolha um SUV pelo seu interior espaçoso, poder e versatibilidade. Prefeito para passar as férias em família ou aventuras off-road.</p>
+          <h2 className="font-shoulders">Suvs</h2>
+          <p className="font-lexend">Escolha um SUV pelo seu interior espaçoso, poder e versatibilidade. Prefeito para passar as férias em família ou aventuras off-road.</p>
           <a href="#">Ver mais</a>
         </div>
         <div className="bg-dark-green">
           <img src={luxuosos} alt="carros luxuosos" />
-          <h2>Luxuosos</h2>
-          <p>Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
+          <h2 className="font-shoulders">Luxuosos</h2>
+          <p className="font-lexend">Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
           <a href="#">Ver mais</a>
         </div>
       </section>
